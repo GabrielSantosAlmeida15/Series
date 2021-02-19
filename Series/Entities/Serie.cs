dddff4265998765cf98bb428cfae4875ff5fa841
Gabriel S Almeida <gabrielsantosalmeida15@gmail.com>
2021-02-19 15:27:15 -0300

    construtor e metodos da classe serie criados

--- Series/Entities/Serie.cs
+++ Series/Entities/Serie.cs
@@ -2,9 +2,43 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Series.Entities
+namespace Series
 {
-    class Serie
+    class Serie : EntidadeBase
     {
+        private Genero Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+
+        public Serie(int id, Genero  genero, string  titulo, string descricao, int ano)
+        {
+            Id = id;
+            Genero = genero;
+            Titulo = titulo;
+            Descricao = descricao;
+            Ano = ano;
+        }
+
+        public string RetornaTitulo()
+        {
+            return Titulo;
+        }
+
+        public int RetornaId()
+        {
+            return Id;
+        }
+
+        public override string ToString()
+        {
+            return "Genero" + Genero + Environment.NewLine +
+                    "Titulo" + Titulo + Environment.NewLine +
+                    "Descrição" + Descricao + Environment.NewLine +
+                    "Ano" + Ano + Environment.NewLine;
+                
+
+
+        }
     }
 }
--- Series/Enums/Genero.cs
+++ Series/Enums/Genero.cs
@@ -2,9 +2,22 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Series.Enums
+namespace Series
 {
-    class Genero
+    enum Genero
     {
+        Acao = 1,
+        Aventura = 2,
+        Comedia = 3,
+        Documentario = 4,
+        Drama = 5,
+        Espionagem = 6,
+        Faroeste = 7,
+        Fantasia = 8,
+        Ficcao_Cientifica = 9,
+        Musical = 10,
+        Romance = 11,
+        Suspense = 12,
+        Terror = 13
     }
 }
