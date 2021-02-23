4e976c66fe1207d50435d66477103cb50381d944
Gabriel S Almeida <gabrielsantosalmeida15@gmail.com>
2021-02-23 11:38:07 -0300

    Metodos de SerieRepositorio implementados

--- Series/Entities/SerieRepositorio.cs
+++ Series/Entities/SerieRepositorio.cs
@@ -1,10 +1,41 @@
-﻿using System;
+﻿using Series.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Series.Entities
 {
-    class SerieRepositorio
+    class SerieRepositorio : IRepositorio<Serie>
     {
+        private List<Serie> ListaSerie = new List<Serie>();
+        public void Atualiza(int id, Serie objeto)
+        {
+            ListaSerie[id] = objeto;
+        }
+
+        public void Exclui(int id)
+        {
+            ListaSerie[id].Excluir();
+        }
+
+        public void Insere(Serie objeto)
+        {
+            ListaSerie.Add(objeto);
+        }
+
+        public List<Serie> Lista()
+        {
+            return ListaSerie;
+        }
+
+        public int ProximoId()
+        {
+            return ListaSerie.Count;
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            return ListaSerie[id];
+        }
     }
 }
--- Series/Interfaces/IRepositorio.cs
+++ Series/Interfaces/IRepositorio.cs
@@ -4,7 +4,13 @@ using System.Text;
 
 namespace Series.Interfaces
 {
-    interface IRepositorio
+    interface IRepositorio<T>
     {
+        List<T> Lista();
+        T RetornaPorId(int id);
+        void Insere(T Entidade);
+        void Exclui(int id);
+        void Atualiza(int id, T entidade);
+        int ProximoId();
     }
 }
